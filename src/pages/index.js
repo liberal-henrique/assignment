203b97761b60f0cdaebb197b6ae41a8061a21fdb
roster <rmyoster@gmail.com>
2022-06-07 08:37:34 +0200

    feat: add first exercise

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,33 @@
 import React from "react"
 import { useSelector } from "react-redux"
-import { Box } from "@mui/material"
+import { Box, Button, Typography, Container } from "@mui/material"
 
+// TODO: create and bind redux actions to modify the counter
 const IndexPage = () => {
   const counter = useSelector(state => state.count)
   return (
-    <Box>
-      {counter}
-    </Box>
+    <Container>
+      <Typography variant="h3" align="center">
+        Counter management
+      </Typography>
+      <Box mt={4} display="flex" justifyContent="center" flexDirection="column"
+        alignItems="center">
+        <Typography>
+          Counter value : {counter}
+        </Typography>
+        <Box mt={1}>
+          <Button variant="contained">
+            Increment
+          </Button>
+          <Button variant="contained" sx={{ mx: 2 }}>
+            Decrement
+          </Button>
+          <Button color="secondary" variant="contained">
+            Reset
+          </Button>
+        </Box>
+      </Box>
+    </Container >
   )
 }
 
